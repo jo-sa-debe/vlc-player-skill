99cfe76ecf9296d691ef6e808bfe5c9be071fb0f
jo-sa-debe <sauwenjohan@hotmail.com>
2020-07-01 15:31:17 +0200

    init

--- __init__.py
+++ __init__.py
@@ -1,13 +1,29 @@
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft import MycroftSkill, intent_file_handler, intent_handler
+from mycroft.messagebus import Message
+from mycroft.skills.audioservice import AudioService
+from mycroft.skills.common_play_skill import CommonPlaySkill, CPSMatchLevel
+from mycroft.audio.services import vlc
+import random
+import os
+from pathlib import Path
 
-
-class VlcPlayer(MycroftSkill):
+class VlcPlayer(CommonPlaySkill):
     def __init__(self):
-        MycroftSkill.__init__(self)
+        super().__init__(name="vlc-player")
 
-    @intent_file_handler('player.vlc.intent')
-    def handle_player_vlc(self, message):
-        self.speak_dialog('player.vlc')
+    def initialize(self): 
+        super().initialize()
+
+    def CPS_match_query_phrase(self, phrase):
+        level = CPSMatchLevel.GENERIC
+        if phrase == "vlc-player":
+            self.speak("query phrase : handled by vlc-player")
+        else:
+            self.speak("query phrase : " + str(phrase))
+        return phrase, level
+
+    def CPS_start(self, phrase, data):
+        pass
 
 
 def create_skill():
