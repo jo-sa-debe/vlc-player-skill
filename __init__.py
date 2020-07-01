e9a5248963036ddea40221ea7d23e879f3653f1d
jo-sa-debe <sauwenjohan@hotmail.com>
2020-07-01 18:17:15 +0200

    init

--- __init__.py
+++ __init__.py
@@ -2,7 +2,8 @@ from mycroft import MycroftSkill, intent_file_handler, intent_handler
 from mycroft.messagebus import Message
 from mycroft.skills.audioservice import AudioService
 from mycroft.skills.common_play_skill import CommonPlaySkill, CPSMatchLevel
-from mycroft.audio.services import vlc
+#from mycroft.audio.services import vlc
+import vlc
 import random
 import os
 from pathlib import Path
@@ -13,6 +14,25 @@ class VlcPlayer(CommonPlaySkill):
 
     def initialize(self): 
         super().initialize()
+        self.instance = vlc.Instance()
+        # default track lists
+        self.list_name = {}
+        self.list_name["default"] = "_default"
+        # list player
+        self.list_player = self.instance.media_list_player_new()
+        # player
+        self.player = self.instance.media_player_new()
+        self.list_player.set_media_player(self.player)
+        # add current track list 
+        self.track_lists = []
+        self.track_lists = self.vlc_add_list_to_lists(self.track_lists, self.list_name["default"])
+        self.track_lists = self.vlc_add_local_folder_to_list('/home/jsauwen/Musik',self.track_lists,self.list_name["default"])
+        self.list_player.set_media_list(self.track_lists[self.list_name["default"]])
+        # events
+        self.vlc_events = self.player.event_manager()
+        self.vlc_list_events = self.list_player.event_manager()
+        self.vlc_events.event_attach(vlc.EventType.MediaPlayerPlaying, self.vlc_start_track, 1)
+        self.vlc_list_events.event_attach(vlc.EventType.MediaListPlayerPlayed, self.vlc_queue_ended, 0)
 
     def CPS_match_query_phrase(self, phrase):
         level = CPSMatchLevel.GENERIC
@@ -20,9 +40,46 @@ class VlcPlayer(CommonPlaySkill):
             self.speak("query phrase : handled by vlc-player")
         else:
             self.speak("query phrase : " + str(phrase))
-        return phrase, level
+        return (phrase, level)
 
     def CPS_start(self, phrase, data):
+        self.vlc_play()
+        pass
+
+    def vlc_add_list_to_lists(self, lists, list_name):
+        lists[list_name] = self.instance.media_list_new()
+        return lists
+
+    def vlc_add_local_folder_to_list(self, folder, lists, list_name):
+        if list_name in lists:
+            for dirpath, dirnames, filenames in os.walk(folder):
+                for file in filenames:
+                    track_path = Path(dirpath)
+                    track_path = track_path / file
+                    lists = self.vlc_add_track_to_list(str(track_path.resolve()), lists, list_name)
+        return lists
+
+    def vlc_start_track(self, data, other):
+        pass
+
+    def vlc_queue_ended(self, data, other):
+        pass
+
+    def vlc_add_track_to_list(self, track, lists, list_name):
+        if list_name in lists:
+            lists[list_name] = self.instance.media_new(track)
+        return lists
+
+    def vlc_remove_track_from_list(self, track, list):
+        pass
+
+    def vlc_play(self):
+        self.list_player.play()
+        pass
+
+    def vlc_stop(self):
+        if self.player.is_playing():
+            self.list_player.stop()
         pass
 
 
