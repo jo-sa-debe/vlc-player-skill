8ed02ecdd7454a9489f8aa00546f51c8a20b2861
jo-sa-debe <sauwenjohan@hotmail.com>
2020-07-01 23:48:45 +0200

    init

--- __init__.py
+++ __init__.py
@@ -106,7 +106,7 @@ class VlcPlayer(CommonPlaySkill):
                     track_path = track_path / file
                     self.speak(str(track_path.resolve()))
                     lists = self.vlc_add_track_to_list(str(track_path.resolve()), lists, list_name)
-        self.speak(str(folder))       
+        self.speak(str(list_name))       
         return lists
 
     def vlc_start_track(self, data, other):
